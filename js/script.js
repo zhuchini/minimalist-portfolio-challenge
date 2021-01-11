c921f21eab92fcba829dd8665730b9126de32b0c
Michael Zhu <michaelzhu63@hotmail.com>
2021-01-11 18:21:30 -0500

    Active Link Highlight complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-const currentActiveLinkIndex = -1;
-
 const headerContentMarkup = `
     <div class="logo">
       <svg xmlns="http://www.w3.org/2000/svg" width="61" height="32"><path fill="#33323D" fill-rule="evenodd" d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"/></svg>
     </div>
     <div class="menu">
-      <a href="index.html?linkindex=1" class="link">Home</a>
-      <a href="portfolio.html?linkindex=2" class="link">Portfolio</a>
-      <a href="contact.html?linkindex=3" class="link">Contact Me</a>
+      <a href="index.html?linkindex=0" class="link">Home</a>
+      <a href="portfolio.html?linkindex=1" class="link">Portfolio</a>
+      <a href="contact.html?linkindex=2" class="link">Contact Me</a>
       <i class="fas fa-bars"></i>
     </div>
 `
@@ -17,9 +15,9 @@ const footerContentMarkup = `
       <svg xmlns="http://www.w3.org/2000/svg" width="61" height="32"><path fill="#FAFAFA" fill-rule="evenodd" d="M60.082 5.878L44.408 32 28.735 5.878h31.347zM15.673 0l15.674 26.122H0L15.673 0z"/></svg>
     </div>
     <div class="menu-2">
-      <a href="index.html?linkindex=1" class="link-2">Home</a>
-      <a href="portfolio.html?linkindex=2" class="link-2">Portfolio</a>
-      <a href="contact.html?linkindex=3" class="link-2">Contact Me</a>
+      <a href="index.html?linkindex=0" class="link-2">Home</a>
+      <a href="portfolio.html?linkindex=1" class="link-2">Portfolio</a>
+      <a href="contact.html?linkindex=2" class="link-2">Contact Me</a>
     </div>
   </div>
   <div class="right-footer">
@@ -42,22 +40,25 @@ function getParamByKey(key) {
   const params = new URLSearchParams(document.location.search.substring(1));
   let value = params.get(key);
 
-  console.log(value);
+  // console.log(value ? value : 0);
+  return value ? value : 0
 }
 
-getParamByKey('linkindex');
+// let activeIndex = Number.parseInt(getParamByKey('linkindex'));
+// let testArr = ['test', '2', 'three'];
 
-// document.querySelectorAll('.link').forEach(item => {
-//   item.addEventListener('click', event => {
-//     // event.classList.add('active-page');
-//   })
-// });
+// testArr.forEach((val, index) => console.log(index === activeIndex))
 
-// $(document).ready(function() {
-//   // 1st click you set the previous and current to the same
-//   // on next click remove active from past index element, set active to currently clicked
-//   $("a").click(function() {
-//     $("a.active-page").addClass("active-page");
-//     $(this).addClass("active-page");
-//   });
-// });
+function highlightActiveLink() {
+  // call getParamByKey('linkindex')
+  const activeLinkIndex = Number.parseInt(getParamByKey('linkindex'));
+
+  document.querySelectorAll('.link').forEach((linkNode, index) => {
+    if (activeLinkIndex === index) {
+      // add active-page to proper link node
+      linkNode.classList.add('active-page')
+    }
+  });
+}
+
+highlightActiveLink();
